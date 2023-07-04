d841b6d1ae4dfa83e92ce0d628fa66f3914ba42c
phongvo <vophongqt1998@gmail.com>
2023-07-05 00:17:11 +0700

    Change custom value

--- otp_code_view/src/main/java/com/otp/otp_code_view/OptCodeView.kt
+++ otp_code_view/src/main/java/com/otp/otp_code_view/OptCodeView.kt
@@ -2,8 +2,12 @@ package com.otp.otp_code_view
 
 
 import android.content.Context
+import android.content.res.Resources
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
 import android.text.InputFilter
 import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -19,12 +23,62 @@ class VerifyCodeView @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr) {
     private val binding: VerifyOtpCodeViewBinding
     private var onVerifyDone: ((String) -> Unit)? = null
-    private var onTextChanged: ((String) -> Unit)? = null
-
+    private var codeColor: Int
+    private var codeLength: Int
+    private var codeBGColor: Int
+    private var codeStrokeColor: Int
+    private var codeStrokeErrorColor: Int
+    private var codeStrokeWidth: Float
+    private var codeErrorColor: Int
+    private var codeDoneStrokeColor: Int
+    private var codeRadius: Float
 
     init {
         binding = VerifyOtpCodeViewBinding.inflate(LayoutInflater.from(context))
         addView(binding.root)
+        val attributes = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.VerifyCodeView,
+            defStyleAttr,
+            defStyleAttr
+        )
+        codeLength = attributes.getInteger(
+            R.styleable.VerifyCodeView_code_length,
+            6
+        )
+        codeStrokeWidth = attributes.getDimension(
+            R.styleable.VerifyCodeView_code_stroke_width,
+            1 * ratioDpToPixels
+        )
+        codeRadius = attributes.getDimension(
+            R.styleable.VerifyCodeView_code_radius,
+            12 * ratioDpToPixels
+        )
+        codeColor = attributes.getColor(
+            R.styleable.VerifyCodeView_code_color,
+            ContextCompat.getColor(context, R.color.color_f2f2f2)
+        )
+        codeBGColor = attributes.getColor(
+            R.styleable.VerifyCodeView_code_background_color,
+            ContextCompat.getColor(context, R.color.color_0af1f6f9)
+        )
+        codeStrokeColor = attributes.getColor(
+            R.styleable.VerifyCodeView_code_background_color,
+            ContextCompat.getColor(context, R.color.color_b3fec70a)
+        )
+        codeStrokeErrorColor = attributes.getColor(
+            R.styleable.VerifyCodeView_code_background_color,
+            ContextCompat.getColor(context, R.color.color_ef4d42)
+        )
+        codeErrorColor = attributes.getColor(
+            R.styleable.VerifyCodeView_code_error_color,
+            ContextCompat.getColor(context, R.color.color_ef4d42)
+        )
+        codeDoneStrokeColor = attributes.getColor(
+            R.styleable.VerifyCodeView_code_done_stroke_color,
+            ContextCompat.getColor(context, R.color.color_fec70a)
+        )
+        attributes.recycle()
         handleInputCode()
     }
 
@@ -35,10 +89,13 @@ class VerifyCodeView @JvmOverloads constructor(
         mainEditText.filters = arrayOf(InputFilter.LengthFilter(listTextView.size))
         mainEditText.setOnClickListener { mainEditText.setSelection(mainEditText.text.length) }
         mainEditText.doOnTextChanged { text, _, _, _ -> handleShowCode(text, listTextView) }
+        listTextView.forEach {
+            it.setTextColor(codeColor)
+            it.background = getDrawable(codeStrokeColor, false)
+        }
     }
 
     private fun handleShowCode(text: CharSequence?, listTextView: List<TextView>) {
-        onTextChanged?.invoke(text.toString())
         binding.tvCodeInvalid.visibility = GONE
         text.toString().apply {
             handleBackgroundHasCode(listTextView)
@@ -53,26 +110,34 @@ class VerifyCodeView @JvmOverloads constructor(
 
     private fun String.handleBackgroundDoneEnter(listTextView: List<TextView>) {
         (listTextView.indices).forEach {
-            listTextView[it].setBackgroundResource(R.drawable.bg_radius_12_yellow2)
-            listTextView[it].setTextColor(ContextCompat.getColor(context, R.color.color_f2f2f2))
+            listTextView[it].background = getDrawable(codeDoneStrokeColor, true)
+            listTextView[it].setTextColor(codeColor)
             listTextView[it].text = get(it).toString()
         }
     }
 
     private fun String.handleBackgroundNotHasCode(listTextView: List<TextView>) {
         (length until listTextView.size).forEach {
-            listTextView[it].setBackgroundResource(R.drawable.bg_radius_12)
-            listTextView[it].setTextColor(ContextCompat.getColor(context, R.color.color_f2f2f2))
+            listTextView[it].background = getDrawable(codeStrokeColor, false)
+            listTextView[it].setTextColor(codeColor)
             listTextView[it].text = ""
         }
     }
 
     private fun String.handleBackgroundHasCode(listTextView: List<TextView>) {
         (indices).forEach {
-            listTextView[it].setTextColor(ContextCompat.getColor(context, R.color.color_f2f2f2))
+            listTextView[it].setTextColor(codeColor)
+            listTextView[it].background = getDrawable(codeStrokeColor, true)
             listTextView[it].text = get(it).toString()
-            listTextView[it].setBackgroundResource(R.drawable.bg_radius_12_yellow)
         }
+    }
+
+    private fun getDrawable(strokeColor: Int, hasStroke: Boolean): GradientDrawable {
+        val drawable = GradientDrawable()
+        drawable.setColor(codeBGColor)
+        drawable.cornerRadius = codeRadius.toFloat()
+        if (hasStroke) drawable.setStroke(codeStrokeWidth.toInt(), strokeColor)
+        return drawable
     }
 
     private fun getAllViews() =
@@ -101,8 +166,17 @@ class VerifyCodeView @JvmOverloads constructor(
         val views = getAllViews()
         val tvs = views.drop(1).map { it as TextView }
         (tvs.indices).forEach {
-            tvs[it].setBackgroundResource(R.drawable.bg_radius_12_stroke_red)
-            tvs[it].setTextColor(ContextCompat.getColor(context, R.color.color_ef4d42))
+            tvs[it].background = getDrawable(codeStrokeErrorColor, true)
+            tvs[it].setTextColor(codeErrorColor)
         }
+    }
+
+    companion object {
+        @JvmField
+        val ratioDpToPixels =
+            Resources.getSystem().displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT
+
+        @JvmField
+        val ratioPixelsToDp: Float = (1.0 / ratioDpToPixels.toDouble()).toFloat()
     }
 }
